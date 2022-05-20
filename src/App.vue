98611f4474dc934f794a9e7e99713b721e7d2954
s92069870 <s92069870@ousl.lk>
2022-05-20 08:58:16 +0530

    first commit

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <img alt="Vue logo" src="https://image.shutterstock.com/image-photo/golden-bitcoin-on-blue-tech-260nw-1828329179.jpg" >
+  <HelloWorld msg="Welcome to Crypto Page" style="color:black;"/>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+
+</style>
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,80 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+    <h3>What is the cripto currency</h3>
+    <p>A cryptocurrency, crypto-currency, crypto, or coin is a digital currency designed to work as a medium of exchange through a computer network that is not reliant on any central authority, 
+      such as a government or bank, to uphold or maintain it.
+
+Individual coin ownership records are stored in a digital ledger, which is a computerized database using strong cryptography to secure transaction records, to control the creation of additional 
+coins, and to verify the transfer of coin ownership. Despite their name, cryptocurrencies are not considered to be currencies in the traditional sense and while varying treatments have been applied to them, 
+including classification as commodities, securities, as well as currencies, cryptocurrencies are generally viewed as a distinct asset class in practice.Some crypto schemes use validators to maintain the cryptocurrency. 
+In a proof-of-stake model, owners put up their tokens as collateral. In return, they get authority over the token in proportion to the amount they stake. Generally, these token stakers get additional ownership in the
+ token over time via network fees, newly minted tokens or other such reward mechanisms.</p>
+ <img src="https://thumbor.forbes.com/thumbor/fit-in/900x510/https://www.forbes.com/advisor/wp-content/uploads/2021/06/how-to-buy-bitcoin.jpeg">
+
+<p>Cryptocurrency does not exist in physical form (like paper money) and is typically not issued by a central authority.
+ Cryptocurrencies typically use decentralized control as opposed to a central bank digital currency (CBDC). When a cryptocurrency is minted or created prior to issuance or issued by a single issuer, 
+ it is generally considered centralized. When implemented with decentralized control, each cryptocurrency works through distributed ledger technology, typically a blockchain, that serves as a public financial transaction database.
+ Traditional asset classes like currencies, commodities, and stocks, as well as macroeconomic factors, have modest exposures to cryptocurrency returns.</p>
+
+<p>A cryptocurrency is a tradable digital asset or digital form of money, built on blockchain technology that only exists online. 
+Cryptocurrencies use encryption to authenticate and protect transactions, hence their name. There are currently over a thousand different cryptocurrencies in the world.
+
+Over the last few years, cryptocurrency prices have risen and then fallen. Crypto marketplaces do not guarantee that an investor is completing a purchase or trade at the optimal price. As a result, 
+many investors take advantage of this by using arbitrage to find the difference in price across several markets.
+
+The first decentralized cryptocurrency was Bitcoin, which first released as open-source software in 2009. Since the release of Bitcoin, many other cryptocurrencies have been created.</p>
+
+<h3>What Can We Do With Bitcoin</h3>
+<p>
+  Bitcoin can be used for online transactions between individuals and nowadays, there are a lot of merchants that accept Bitcoin as a form of 
+  payment unlike before. They range from online retailers like Overstock and Newegg to local shops, bars and restaurants. Bitcoins can also be used to pay for hotels, flights, jewellery, apps and computer parts, or even a college degree.
+</p>
+<h3> History Of Cryptocurrency</h3>
+<p>
+  In 1983, the American cryptographer David Chaum conceived an anonymous cryptographic electronic money called ecash. Later, in 1995, he implemented it through Digicash, an early form of cryptographic electronic payments. 
+  Digicash required user software in order to withdraw notes from a bank and designate specific encrypted keys before it can be sent to a recipient. This allowed the digital currency to be untraceable by the issuing bank, the government, or any third party.</p>
+
+<p>In 1996, the National Security Agency published a paper entitled How to Make a Mint: the Cryptography of Anonymous Electronic Cash, describing a Cryptocurrency system, first publishing it in an MIT mailing listand later in 1997, in The American Law Review (Vol. 46, Issue 4).<br>
+
+
+<br>In 1998, Wei Dai published a description of "b-money", characterized as an anonymous, distributed electronic cash system. Shortly thereafter, Nick Szabo described bit gold. Like Bitcoin and other cryptocurrencies that would follow it, 
+bit gold (not to be confused with the later gold-based exchange, BitGold) was described as an electronic currency system which required users to complete a proof of work function with solutions being cryptographically put together and published.<br>
+
+<br>In 2009, the first decentralized cryptocurrency, Bitcoin, was created by presumably pseudonymous developer Satoshi Nakamoto. It used SHA-256, a cryptographic hash function, in its proof-of-work scheme. In April 2011, Namecoin was created as
+ an attempt at forming a decentralized DNS, which would make internet censorship very difficult. Soon after, in October 2011, Litecoin was released which used scrypt as its hash function instead of SHA-256. Another notable cryptocurrency,
+  Peercoin, used a proof-of-work/proof-of-stake hybrid.<br>
+
+<br>On 6 August 2014, the UK announced its Treasury had commissioned a study of cryptocurrencies, and what role, if any, they could play in the UK economy. The study was also to report on whether regulation should be considered.
+ Its final report was published in 2018, and it issued a consultation on cryptoassets and stablecoins in January 2021.<br>
+
+<br>In June 2021, El Salvador became the first country to accept Bitcoin as legal tender, after the Legislative Assembly had voted 62–22 to pass a bill submitted by President Nayib Bukele classifying the cryptocurrency as such.<br>
+
+<br>In August 2021, Cuba followed with Resolution 215 to recognize and regulate cryptocurrencies such as Bitcoin.<br>
+
+<br>In September 2021, the government of China, the single largest market for cryptocurrency, declared all cryptocurrency transactions illegal, 
+completing a crackdown on cryptocurrency that had previously banned the operation of intermediaries and miners within China.
+</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<style>
+p{
+  text-align: left;
+  text-transform:capitalize;
+  font-size: small;
+  padding-left: 10%;
+}
+
+</style>
+
+
