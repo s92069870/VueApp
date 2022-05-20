a51adee21be55f9fdcf5d94c19966a5406e10946
s92069870 <90398035+s92069870@users.noreply.github.com>
2022-05-20 20:40:57 +0530

    Add files via upload

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,12 @@
-<template>
+<template >
+     <ul>
+      <li><a href="#home" class="active">Home</a></li>
+      <li><a href="#aboutus" >About Us</a></li>
+      <li><a href="#contactus" >Contact US</a></li>
+      <li><a href="News.html" >News</a></li>
+  </ul>
   <img alt="Vue logo" src="https://image.shutterstock.com/image-photo/golden-bitcoin-on-blue-tech-260nw-1828329179.jpg" >
-  <HelloWorld msg="Welcome to Crypto Page" style="color:black;"/>
+  <HelloWorld msg="Welcome to Crypto Page" style="color:whitesmoke;"/>
 </template>
 
 <script>
@@ -12,6 +18,7 @@ export default {
     HelloWorld
   }
 }
+
 </script>
 
 <style>
@@ -23,5 +30,34 @@ export default {
   color: #2c3e50;
   margin-top: 60px;
 }
+            ul{
+                list-style-type: none;
+                margin: 0;
+                padding: 0;
+                overflow: hidden;
+                background-color: #333;
+            }
+            li{
+               float: left;
+            }
+            li a{
+                display: block;
+                color:white;
+                padding: 14px 16px;
+                text-align: center;
+                text-decoration: none;
+            }
+            
+            li a.active{
+                background-color: #4CAF50;
+                color: white;
+            }
+            
+            li a:hover:not(active){
+                background-color: #111;
+            }
+            body{
+              background-color: #111;
+            }
 
 </style>
